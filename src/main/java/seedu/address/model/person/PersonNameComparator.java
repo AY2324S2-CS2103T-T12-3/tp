8324df3cbede9cb7017feb0adeb9ea8c653fe757
tcjazwei <oliver.j.tan@gmail.com>
2024-03-26 11:16:49 +0800

    Resolve merge conflicts

--- src/main/java/seedu/address/model/person/PersonNameComparator.java
+++ src/main/java/seedu/address/model/person/PersonNameComparator.java
@@ -3,11 +3,7 @@ package seedu.address.model.person;
 import java.util.Comparator;
 
 /**
-<<<<<<< .merge_file_cZVOia
  * Class for comparing two {@code Person}s according to the alphabetical order of their {@code Name}s
-=======
- * Class for comparing two Persons according to the alphabetical order of their full names
->>>>>>> .merge_file_CBjWs7
  */
 public class PersonNameComparator implements Comparator<Person> {
     @Override
